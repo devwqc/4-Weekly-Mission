1cd26e6d022e05adb3eb3a48798043882427bb52
devwqc <devwqc@gmail.com>
2024-04-01 02:03:16 +0900

    feat: _app 페이지 Layout 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,24 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '@/styles/globals.css';
+import styles from '@/styles/App.module.css';
+import { Footer } from '@/src/components/Layout/Footer';
+import { Header } from '@/src/components/Layout/Header';
+import { LoginProvider } from '@/src/contexts/LoginContext';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Linkbrary</title>
+      </Head>
+      <LoginProvider>
+        <Header />
+        <main className={styles.main}>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </LoginProvider>
+    </>
+  );
 }
