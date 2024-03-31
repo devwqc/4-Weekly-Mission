d9c8d7cc1e0a8360218f8c2a1adf8d4538038e43
devwqc <devwqc@gmail.com>
2024-04-01 01:10:28 +0900

    feat: api utils 추가

---
+++ src/apis/api.ts
@@ -0,0 +1,71 @@
+import { Link, LinkBase } from '../types/card';
+
+const BASE_URL = 'https://bootcamp-api.codeit.kr/api';
+
+export async function getFolder() {
+  try {
+    const response = await fetch(`${BASE_URL}/sample/folder`);
+    if (!response.ok) {
+      throw new Error('폴더를 불러오는데 실패했습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function getUser(userId: number) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/${userId}`);
+    if (!response.ok) {
+      throw new Error('유저를 불러오는데 실패했습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function getFolders(userId: number) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/${userId}/folders`);
+    if (!response.ok) {
+      throw new Error('폴더 목록을 불러오는데 실패했습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function getLinks(userId: number, folderId: number | null) {
+  const query = `${folderId && 'folderId=' + folderId}`;
+  try {
+    const response = await fetch(`${BASE_URL}/users/${userId}/links?${query}`);
+    if (!response.ok) {
+      throw new Error('링크 목록을 불러오는데 실패했습니다.');
+    }
+    const links = await response.json();
+    const data = links.data?.map((link: LinkBase) => {
+      const { created_at, updated_at, image_source, folder_id } = link;
+      const revertLink: Link = {
+        ...link,
+        createdAt: created_at,
+        updatedAt: updated_at,
+        imageSource: image_source,
+        folderId: folder_id,
+      };
+      return revertLink;
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
