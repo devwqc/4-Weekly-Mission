edb100293ba7ca49372f6a1723bb4cd5e590157e
devwqc <devwqc@gmail.com>
2024-04-01 02:03:50 +0900

    feat: Home page 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
+import styles from '@/styles/Home.module.css';
+import { Signup } from '@/src/components/Signup';
+import { Button } from '@/src/components/Button';
+
 export default function Home() {
   return (
-    <>
-      <h1>Next.js</h1>
-    </>
+    <div className={styles.landing}>
+      <h1>Landing page</h1>
+      <div className={styles.linkContainer}>
+        <div>
+          <Link href="/shared">
+            <Button>shared 페이지</Button>
+          </Link>
+        </div>
+        <div>
+          <Link href="/folder">
+            <Button>folder 페이지</Button>
+          </Link>
+        </div>
+      </div>
+      <Signup />
+    </div>
   );
 }
