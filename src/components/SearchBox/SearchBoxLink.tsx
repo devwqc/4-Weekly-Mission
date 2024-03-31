cc26252d6347b2ec0fce70a3d5a775f108b6a81c
devwqc <devwqc@gmail.com>
2024-04-01 01:33:48 +0900

    feat: SearchBoxLink 컴포넌트 생성

---
+++ src/components/SearchBox/SearchBoxLink.tsx
@@ -0,0 +1,22 @@
+import * as S from './SearchBoxLink.styled';
+import { IconLink } from '../Icon';
+import { Button } from '../Button';
+import { InputProps } from '@/src/types/props';
+
+interface SearchBoxLinkProps extends InputProps {}
+
+function SearchBoxLink(props: SearchBoxLinkProps) {
+  return (
+    <S.SearchLayout>
+      <S.SearchInputBox>
+        <IconLink />
+        <S.SearchInput {...props} />
+      </S.SearchInputBox>
+      <Button type="button" fontSize={14}>
+        추가하기
+      </Button>
+    </S.SearchLayout>
+  );
+}
+
+export default SearchBoxLink;
