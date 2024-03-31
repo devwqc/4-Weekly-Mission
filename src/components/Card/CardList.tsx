165e515906e5c0663a90dfa2705af656dfa1cb23
devwqc <devwqc@gmail.com>
2024-04-01 00:58:44 +0900

    feat: CardList 컴포넌트 생성

---
+++ src/components/Card/CardList.tsx
@@ -0,0 +1,16 @@
+import Card, { CardItem } from './Card';
+import styles from './CardList.module.css';
+
+interface CardListProps {
+  items: CardItem[];
+}
+
+function CardList({ items }: CardListProps) {
+  return (
+    <div className={styles['card-list']}>
+      {items?.map((item) => <Card key={item.id} item={item} />)}
+    </div>
+  );
+}
+
+export default CardList;
